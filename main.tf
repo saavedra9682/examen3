87c3df2fb30320ef7dfb06a66ffcce4ad64ce351
saavedra9682 <saavedra9682@gmail.com>
2022-04-24 16:24:54 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  ## Configuration options
+  uri = "qemu:///system"
+  #alias = "server2"
+  #uri   = "qemu+ssh://root@192.168.100.10/system"
+}
+
